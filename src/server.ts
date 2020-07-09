f3b3a0c16b04049c91757d83a01c04db39d5a6b1
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-09 10:04:47 -0300

    static files route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Routes from './routes';
+import uploadConfig from './config/upload';
 import 'reflect-metadata';
 
 import './database';
@@ -7,6 +8,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
 
 app.listen(3333, () => {
