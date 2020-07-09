328a1c7ecbe6060e19b8f1f090ba1a146080dea6
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-09 10:32:08 -0300

    global exception handler

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -19,22 +19,18 @@ appointmentsRouter.get('/', ensureAuthenticated, async (request, response) => {
 });
 
 appointmentsRouter.post('/', ensureAuthenticated, async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentsService();
+  const createAppointment = new CreateAppointmentsService();
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider_id,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import Routes from './routes';
 import uploadConfig from './config/upload';
-import 'reflect-metadata';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -10,6 +13,22 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(Routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
