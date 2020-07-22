0ebe62213951d58cc37bc74dedfe2dcaca720a4d
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-22 11:52:02 -0300

    feat: create Mail Provider

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(): Promise<void> {}
+}
+
+export default SendForgotPasswordEmailService;
